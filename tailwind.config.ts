dabed3ca80a9e6c9bc42ab328c8718f76f474079
Rafhael Mallorga <rafhaelmallorga@gmail.com>
2025-12-13 19:37:30 -0300

    new: update skeleton

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,5 @@
 import type { Config } from 'tailwindcss';
 
-const config: Config = {
-    theme: {
-        extend: {
-            colors: {
-                text: '#1A1A1A',
-                strokes: '#2B2B2B',
-                button: '#F3701E',
-                one: '#F4ECE4',
-                two: '#E8D8C9',
-            }
-        }
-    }
-}
+const config: Config = {}
+
 export default config;
